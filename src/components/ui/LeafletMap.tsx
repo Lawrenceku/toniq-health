6778890a0bdafacc2f5c38adf9573cddca2825e4
Lawrence <lawrencekunmilola@gmail.com>
2025-12-12 17:51:05 +0100

    add oSMI addredd for map image

--- src/components/ui/LeafletMap.tsx
+++ src/components/ui/LeafletMap.tsx
@@ -71,7 +71,7 @@ export default function LeafletMap({ hospitals, selectedHospital }: LeafletMapPr
         >
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
             />
             {markers.map((hospital) => (
                 <Marker key={hospital.id} position={[hospital.lat, hospital.lng]}>
